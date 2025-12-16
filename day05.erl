bd3f6a3b8388c78c957d3d6de0edb849e4c0dd68
Jesper Eskilson <jesper@eskilson.se>
2025-12-16 21:35:59 +0100

    Day 5 benchmarking

--- day05.erl
+++ day05.erl
@@ -2,16 +2,33 @@
 
 -export([
     solve_part1/1,
-    solve_part2/1]).
+    solve_part2/1,
+    benchmark/1
+]).
 
 solve_part1(Filename) ->
     {Ranges, Ids} = parse(Filename),
     number_of_fresh_ids(Ranges, Ids).
 
-%% wrong 344515880549324
 solve_part2(Filename) ->
     {Ranges, _Ids} = parse(Filename),
     total_fresh_ids(Ranges).
+
+repeat(Fun, 1) -> Fun();
+repeat(Fun, N) -> Fun(), repeat(Fun, N - 1).
+
+benchmark(Filename) ->
+    Repeats = 1000,
+    {T, V} = timer:tc(fun() ->
+        repeat(fun() ->
+            {Ranges, Ids} = parse(Filename),
+            P1 = number_of_fresh_ids(Ranges, Ids),
+            P2 = total_fresh_ids(Ranges),
+            {P1, P2}
+        end, Repeats)
+    end),
+    io:format("Time: ~p usecs~n", [T / Repeats]),
+    V.
 
 parse(Filename) ->
     {ok, Bin} = file:read_file(Filename),
@@ -20,7 +37,6 @@ parse(Filename) ->
             [A1, A2] = split(B, <<"-">>),
             N1 = binary_to_integer(A1),
             N2 = binary_to_integer(A2),
-            true = (N1 =< N2),
             {N1, N2}
         end,
         split(Ranges, <<"\n">>)),
@@ -59,22 +75,20 @@ count_ranges([{A1, A2}|Rest], N) ->
 
 merge_ranges_until_done(Ranges) ->
     SortedRanges = lists:sort(Ranges),
-    Ranges0 = merge_ranges(SortedRanges, []),
+    {Ranges0, Merges} = merge_ranges(SortedRanges, [], 0),
     if
-        length(Ranges0) == length(Ranges) ->
-            Ranges;
-        true ->
-            merge_ranges_until_done(Ranges0)
+        Merges == 0 -> Ranges;
+        true -> merge_ranges_until_done(Ranges0)
     end.
 
-merge_ranges([], Acc) ->
-    Acc;
-merge_ranges([Range], Acc) ->
-    merge_ranges([], [Range|Acc]);
-merge_ranges([{A1,A2}, {B1,B2}|Rest], Acc) when A2 + 1 >= B1 ->
+merge_ranges([], Acc, Merges) ->
+    {Acc, Merges};
+merge_ranges([Range], Acc, Merges) ->
+    merge_ranges([], [Range|Acc], Merges);
+merge_ranges([{A1,A2}, {B1,B2}|Rest], Acc, Merges) when A2 + 1 >= B1 ->
     %% merge range, note that the B range might be fully contained within A, so
     %% take max(A2, B2) as the upper bound.
-    merge_ranges([{A1,max(A2, B2)}|Rest], Acc);
-merge_ranges([{A1,A2}, {B1,B2}|Rest], Acc) when A2 + 1 < B1 ->
+    merge_ranges([{A1,max(A2, B2)}|Rest], Acc, Merges + 1);
+merge_ranges([{A1,A2}, {B1,B2}|Rest], Acc, Merges) when A2 + 1 < B1 ->
     %% disjoint range
-    merge_ranges([{B1,B2}|Rest], [{A1, A2}|Acc]).
+    merge_ranges([{B1,B2}|Rest], [{A1, A2}|Acc], Merges).
